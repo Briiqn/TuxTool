38d1485bcf6cc3bac3e49eb1b386f8a38cb75613
Q-dev <82529475+QV-dev@users.noreply.github.com>
2021-04-14 21:02:41 -0600

    bug fixed, detection much more reliable

--- TuxTool/generic/minecraftgeneric2.sh
+++ TuxTool/generic/minecraftgeneric2.sh
@@ -14,8 +14,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with TuxTool.  If not, see <https://www.gnu.org/licenses/>.
 ProcessCheck=$(ps -ef | grep 'java' | grep 'minecraft' | sort -g | sed -r 's/^([^.]+).*$/\1/; s/^[^0-9]*([0-9]+).*$/\1/')
-CheckDeletedFile=$(cat /proc/$ProcessCheck/task/$ProcessCheck | grep deleted | grep .minecraft)
-if [ -z !$CheckDeletedFile ]; then
+CheckDeletedFile=$(cat /proc/$ProcessCheck/task/$ProcessCheck/maps | grep deleted | grep mods)
+if [[ $CheckDeletedFile == *'deleted'* ]]; then
 echo -e 'User has been flagged for generic cheat B' >> /tmp/scanresults.txt 
 fi
-# revolutionary linux method, is able to detect any mod cheat if it's deleted
+# revolutionary linux method, is able to detect any mod cheat if its deleted
