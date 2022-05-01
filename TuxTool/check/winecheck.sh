25f2a4739d2a345d337a2a60fbcc8ffe0443d9bd
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-04-30 23:06:50 -0400

    better intergration

--- TuxTool/check/winecheck.sh
+++ TuxTool/check/winecheck.sh
@@ -11,14 +11,16 @@ sleep 1
 echo minecraft under wine has been launched with the arguments below
 echo ___________________________________________________
 echo $mcproc
-rm /tmp/TuxTool.tmp
-echo $mcproc >> /tmp/TuxTool.tmp
-if grep -q "javaagent"  /tmp/TuxTool.tmp; then
+sudo echo -e __________________________ >> /tmp/scanresults.txt
+sudo echo Wine Check >> /tmp/scanresults.txt
+if grep -q "javaagent"  /tmp/scanresults.txt; then
 echo Minecraft has been launched with a javaagent, $USER is most likely cheating
-echo Minecraft is running under wine, please proceed to use normal ss methods, i dont think echo will work tho
+sudo echo -e Minecraft is running under wine, please proceed to use normal ss methods, i dont think echo will work tho >> /tmp/scanresults.txt
+sudo echo -e Failed Wine and Wine Java Agent test >> /tmp/scanresults.txt
 else
-echo No Java Agents detected
-echo Minecraft is running under wine, please proceed to use normal ss methods, i dont think echo will work tho
+sudo echo -e Passed Wine agent test >> /tmp/scanresults.txt
+sudo echo -e Failed Wine test >> /tmp/scanresults.txt
+sudo echo -e Minecraft is running under wine, please proceed to use normal ss methods, i dont think echo will work tho >> /tmp/scanresults.txt
 fi
 else
 echo Minecraft is not running under wine... exiting
