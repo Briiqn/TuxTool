cc1f5a2b3da6745de6422726d489c796dc7eafdd
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-05-01 22:29:34 -0400

    Update NativeJavaAgentCheck.sh

--- TuxTool/check/NativeJavaAgentCheck.sh
+++ TuxTool/check/NativeJavaAgentCheck.sh
@@ -2,7 +2,7 @@
  mcproc=$(ps aux | grep java)
 echo $mcproc >> /tmp/NativeAgent.tmp
  if grep -q "javaagent" /tmp/NativeAgent.tmp; then
-sudo echo "User Failed Native Java Agent Test (Check C)" >> /tmp/scanresults.txt
+sudo echo "User Failed Native Java Agent Test (Check D)" >> /tmp/scanresults.txt
 else
 echo .
 fi
