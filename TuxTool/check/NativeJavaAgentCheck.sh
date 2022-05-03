81a6c990556bc315788f02245e9221cf3c3e07ee
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-05-03 18:24:59 -0400

    Delete TuxTool directory

--- TuxTool/check/NativeJavaAgentCheck.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
- mcproc=$(ps aux | grep java)
-echo $mcproc >> /tmp/NativeAgent.tmp
- if grep -q "javaagent" /tmp/NativeAgent.tmp; then
-sudo echo "User Failed Native Java Agent Test (Check E)" >> /tmp/scanresults.txt
-else
-echo .
-fi
-rm /tmp/NativeAgent.tmp
