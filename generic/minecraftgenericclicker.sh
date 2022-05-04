1d063dbbebcc1ab2869e75300830cbbf16fca967
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-05-03 20:36:05 -0400

    Update minecraftgenericclicker.sh

--- generic/minecraftgenericclicker.sh
+++ generic/minecraftgenericclicker.sh
@@ -7,7 +7,7 @@ if [ -f "/usr/bin/xdotool" ]; then
 echo "User has Generic Macro Tool A Installed" >> /tmp/scanresults.txt
 fi
 if [ -f "/usr/bin/click4ever" ]; then
-echo "User has Generic AutoClicker C Installed" >> /tmp/scanresults.txt
+echo "User has Generic AutoClicker B Installed" >> /tmp/scanresults.txt
 fi
 if [ -f "/usr/bin/clicker" ]; then
 echo "User has Generic AutoClicker C Installed" >> /tmp/scanresults.txt
