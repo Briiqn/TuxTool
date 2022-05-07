3b48c37a349dd5f41b6815a7befe2a437abaf417
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-05-06 23:04:23 -0400

    fix funny

--- generic/minecraftgeneric4.sh
+++ generic/minecraftgeneric4.sh
@@ -3,8 +3,17 @@ check1=$(ps -p `pidof java` -o etimes= )
 
 check2=$(expr `date +%s` - `stat -c %Y /home/$USER/.minecraft/mods/*/`)
 
+if [ $check2 == ]; then
+check2=$(echo "ERROR")
+else
+echo .
+fi
+
 if [ $check1 -gt $check2 ]; then
 echo "User Modified Mods Folder After Minecraft was launched (Generic 4)" >> /tmp/scanresults.txt
 else
-echo "Minecraft was launched $check1 seconds ago & user""'""s mods folder was last modified" $check2 "seconds ago" >> /tmp/scanresults.txt
+echo "Minecraft was launched"$check1 "seconds ago & user""'""s mods folder was last modified" $check2 "seconds ago" >> /tmp/scanresults.txt
+fi
+if [ $check2 = "ERROR" ]; then
+echo "The Error May be due to the user not having a existing mods folder, check if the user is running forge/fabric" >> /tmp/scanresults.txt
 fi
