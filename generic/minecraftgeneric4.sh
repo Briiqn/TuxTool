1cd6a5097364aeee9e329ef2a014439f5fe12c10
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-05-06 23:15:56 -0400

    ok i really fix funne

--- generic/minecraftgeneric4.sh
+++ generic/minecraftgeneric4.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 check1=$(ps -p `pidof java` -o etimes= )
 
-check2=$(expr `date +%s` - `stat -c %Y /home/$USER/.minecraft/mods/*/`)
+check2=$(expr `date +%s` - `stat -c %Y /home/$USER/.minecraft/mods/*`)
+check3=$(expr `date +%s` - `stat -c %Y /home/$USER/.minecraft/mods/*/*`)
 
 if [ $check2 == ]; then
 check2=$(echo "ERROR")
@@ -11,6 +12,11 @@ fi
 
 if [ $check1 -gt $check2 ]; then
 echo "User Modified Mods Folder After Minecraft was launched (Generic 4)" >> /tmp/scanresults.txt
+else
+echo "Minecraft was launched"$check1 "seconds ago & user""'""s mods folder was last modified" $check2 "seconds ago" >> /tmp/scanresults.txt
+fi
+if [ $check1 -gt $check3 ]; then
+echo "User Modified Subdirectories in Mods Folder After Minecraft was launched (Generic 4A)" >> /tmp/scanresults.txt
 else
 echo "Minecraft was launched"$check1 "seconds ago & user""'""s mods folder was last modified" $check2 "seconds ago" >> /tmp/scanresults.txt
 fi
