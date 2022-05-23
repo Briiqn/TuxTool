f1f40addcebc55327410f272083a776f17b16184
Briiqn <82249811+Briiqn@users.noreply.github.com>
2022-05-23 07:26:19 -0400

    Update minecraftgeneric4.sh

--- generic/minecraftgeneric4.sh
+++ generic/minecraftgeneric4.sh
@@ -6,16 +6,11 @@ check3=$(expr `date +%s` - `stat -c %Y /home/$(whoami)/.minecraft/mods//`)
 
 if [ $check2 == ]; then
 check2=$(echo "ERROR")
-else
-echo .
-fi
 
-if [ $check1 -gt $check2 ]; then
+elif [ $check1 -gt $check2 ]; then
 echo "User Modified Mods Folder After Minecraft was launched (Generic 4)" >> /tmp/scanresults.txt
-else
 echo "Minecraft was launched"$check1 "seconds ago & user""'""s mods folder was last modified" $check2 "seconds ago" >> /tmp/scanresults.txt
-fi
-if [ $check1 -gt $check3 ]; then
+elif [ $check1 -gt $check3 ]; then
 echo "User Modified Subdirectories in Mods Folder After Minecraft was launched (Generic 4A)" >> /tmp/scanresults.txt
 else
 echo "Minecraft was launched"$check1 "seconds ago & user""'""s mods folder was last modified" $check2 "seconds ago" >> /tmp/scanresults.txt
